62d38a54a38ceb55f37797015b8ef5aa8c74e2db
esolares <solarese@uci.edu>
2014-08-22 12:27:21 -0700

    simple loop
    checks i and i + 1  only

--- simplesorting1.py
+++ simplesorting1.py
@@ -6,4 +6,21 @@ Created on Fri Aug 22 11:11:58 2014
 """
 myList = list()
 myList = [5, 7, 13, 6, 4, 9, 10, 25, 3, 11, 50, 8, 1]
-print myList
+print(str(myList))
+
+counter = 0
+
+#while counter < len(myList):
+#    curr = myList[counter]
+    
+easyList = [3,2,1,4,5]
+easyLen = len(easyList)
+counter = 0
+while counter < easyLen-1:
+    if easyList[counter] > easyList[counter+1]:
+        temp = easyList[counter]
+        easyList[counter] = easyList[counter+1]
+        easyList[counter+1] = temp
+    counter = counter + 1
+
+print(str(easyList))
